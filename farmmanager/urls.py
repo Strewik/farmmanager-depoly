3c6b595db9f83afb297342adb813e6bcc5bb51c5
steven <kawooyastevenug@gmail.com>
2020-09-18 16:58:32 +0300

    Debug true

--- farmmanager/urls.py
+++ farmmanager/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('farmfront.urls')),
-    path('', include('farmback.urls'))
+    path('', include('farmback.urls')),
+    # path('', include('farmfront.urls')),
+
 ]
