bc105c7c4e7f16e0ed7a5f0d88bd6e92a9c1e8fd
Irenyak1 <inyakate@gmail.com>
2021-04-08 15:59:10 +0300

    Comments out all swagger settings

--- farmmanager/urls.py
+++ farmmanager/urls.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="FARM MANAGER API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://farmmanager-api.herokuapp.com/policies/terms/",
-        contact=openapi.Contact(email="contact@expenses.local"),
-        license=openapi.License(name="Test License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="FARM MANAGER API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://farmmanager-api.herokuapp.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@expenses.local"),
+#         license=openapi.License(name="Test License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 
 urlpatterns = [
@@ -25,11 +25,11 @@ urlpatterns = [
     # path('social_auth/', include(('social_auth.urls', 'social_auth'),
     #                              namespace="social_auth")),
     path('farmback/', include('farmback.urls')),
-    path('', schema_view.with_ui('swagger',
-                                 cache_timeout=0), name='schema-swagger-ui'),
+    # path('', schema_view.with_ui('swagger',
+    #                              cache_timeout=0), name='schema-swagger-ui'),
 
-    path('api/api.json/', schema_view.without_ui(cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-                                       cache_timeout=0), name='schema-redoc'),
+    # path('api/api.json/', schema_view.without_ui(cache_timeout=0),
+    #      name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc',
+    #                                    cache_timeout=0), name='schema-redoc'),
 ]
